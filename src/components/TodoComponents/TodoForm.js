ce252b43c226bd8aa9d506c741e70e9b197d93f7
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-13 14:50:02 -0600

    TodoForm skeleton and fixes errors. list is rendering fine so far

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component
+{
+    constructor()
+    {
+        super();
+        this.state = {newItem: ''}
+    }
+
+    handleChange = event =>
+    {
+
+    }
+
+    handleSubmit = event =>
+    {
+
+    }
+
+    render()
+    {
+        return (
+            <form>
+
+            </form>
+        );
+    }
+}
+export default TodoForm;
