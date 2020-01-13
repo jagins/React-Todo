4337393cca653f388bd18904d784e9dbeb4a66fd
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-13 14:23:50 -0600

    Added in dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+const data = [
+  {
+    id: 1,
+    name: 'Learn setState()',
+    isFinished: false
+  },
+
+  {
+    id: 2,
+    name: 'Style App',
+    isFinished: false
+  },
+
+  {
+    id: 3,
+    name: 'Learn React',
+    isFinished: false
+  }
+]
+
+class App extends Component 
+{
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
+  render() 
+  {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
