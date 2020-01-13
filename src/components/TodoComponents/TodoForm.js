0460bd3d858d43be50f8ec1bcede7323d69f2f20
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-13 14:56:53 -0600

    Added in functions for todoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -10,19 +10,26 @@ class TodoForm extends Component
 
     handleChange = event =>
     {
-
+        this.setState({newItem: event.target.value});
     }
 
     handleSubmit = event =>
     {
-
+        event.preventDefault();
+        this.props.addNewTodo(this.state.newItem);
     }
 
     render()
     {
         return (
-            <form>
-
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    type='text' 
+                    name='item' 
+                    value={this.state.newItem} 
+                    onChange={this.handleChange}
+                />
+                <button>Add New Todo</button>
             </form>
         );
     }
